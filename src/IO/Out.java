13ad619d44b5441b16929d7cc726f6d260644886
ImJuAR <j.a.reiche@t-online.de>
2022-12-28 20:07:37 +0100

    Coloring NextField in Out.java

--- src/IO/Out.java
+++ src/IO/Out.java
@@ -1,10 +1,19 @@
 package IO;
 
 import Game.Board;
+import util.Colors;
 
 public class Out {
     public void print(Board board) {
-        System.out.println("   a  b  c  |  d  e  f  |  g  h  i");
+        switch (board.getNextActiveField().getX2()) {
+            case 1 ->
+                    System.out.println(Colors.GREEN_BRIGHT + "   a  b  c  " + Colors.RESET + "|  d  e  f  |  g  h  i");
+            case 2 ->
+                    System.out.println("   a  b  c  |" + Colors.GREEN_BRIGHT + "  d  e  f  " + Colors.RESET + "|  g  h  i");
+            case 3 ->
+                    System.out.println("   a  b  c  |  d  e  f  |" + Colors.GREEN_BRIGHT + "  g  h  i" + Colors.RESET);
+        }
+
 
         for (int i = 0; i < 9; i++) {
             int line = i + 1;
@@ -12,17 +21,19 @@ public class Out {
             if (i == 3 || i == 6) {
                 System.out.println("-----------------------------------");
             }
-            System.out.print(line + " ");
+
+            if (Math.ceil(i / 3) + 1 == board.getNextActiveField().getX1()) {
+                System.out.print(Colors.GREEN_BRIGHT + line + Colors.RESET + " ");
+            } else {
+                System.out.print(line + " ");
+            }
+
             for (int j = 0; j < 9; j++) {
-                String content = " ";
-                switch(board.getContent()[i][j]){
-                    case 0:
-                        content = "X";
-                        break;
-                    case 1:
-                        content = "O";
-                        break;
-                }
+                String content = switch (board.getContent()[i][j]) {
+                    case 0 -> "X";
+                    case 1 -> "O";
+                    default -> " ";
+                };
 
                 System.out.print("[" + content + "]");
                 if (j == 2 || j == 5) {
